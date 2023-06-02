5b974050e459f388bc5baa0bfa261222b78320dc
Zach <zkdoran@gmail.com>
2023-06-02 18:49:53 -0400

    Added Grand Total html and js programs.

---
+++ js/application.js
@@ -0,0 +1,28 @@
+var updateTotal = function (ele) {
+  var quant = parseFloat($(ele).find('.quant input').val());
+  var price = parseFloat($(ele).children('.price').text());
+
+  var total = price * quant;
+  $(ele).children('.total').html(total);
+
+  return total;
+}
+
+var sum = function (acc, x) { return acc + x; };
+
+var finalTotal = function () {
+  var subTotal = [];
+
+  $('tbody tr').each(function (i, ele) {
+    var newTotal = updateTotal(ele);
+    subTotal.push(newTotal);
+  });
+
+  var grandTotal = subTotal.reduce(sum);
+
+  $('#grandTotal').html(grandTotal);
+}
+
+$(document).ready(function () {
+  finalTotal();
+});
